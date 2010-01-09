d46cb363c047cb4ccaa31dae7ab5811f8463d63a
Oskar Nilsson <osn@ksu.se>
2010-01-09 07:25:27 +0000

    Bug fixes
    git-svn-id: file:///home/svnrepo/sbh/trunk@29 898ca85e-fc40-43f0-9d85-aa2c6ea4f529

--- settings.py
+++ settings.py
@@ -75,7 +75,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #'/home/oskar/Projects/sbh/templates'
     os.path.join(ROOT_PATH, 'templates')
 )
 
